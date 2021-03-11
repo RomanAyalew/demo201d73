4f60f8fdb02f0192ef4c6d1f67f59bcc462e0d93
SkylerJohnson102020 <basseuph@gmail.com>
2021-03-11 15:38:36 -0800

    Add parseInt line 132

--- js/app.js
+++ js/app.js
@@ -15,107 +15,109 @@
 // }
 
 
-'use strict';
+// 'use strict';
 
 let score = 0;
 
-function getName(){
-  let userName = prompt('Please Enter your Name: ');
-  alert('Welcome to my page, ' + userName + '!');
-}
+// function getName(){
+//   let userName = prompt('Please Enter your Name: ');
+//   alert('Welcome to my page, ' + userName + '!');
+// }
 
-getName();
+// getName();
 
-function guessAge() {
-  let ageAnswer = prompt('What is my age?');
+// function guessAge() {
+//   let ageAnswer = prompt('What is my age?');
 
-  if (ageAnswer > 30) {
-    alert('Too High');
-  } else if (ageAnswer < 30) {
-    alert('Too Low');
-  } else {
-    alert('Correct!');
-    score += 1;
-  }
-}
+//   if (ageAnswer > 30) {
+//     alert('Too High');
+//   } else if (ageAnswer < 30) {
+//     alert('Too Low');
+//   } else {
+//     alert('Correct!');
+//     score += 1;
+//   }
+// }
 
-guessAge();
+// guessAge();
 
-function guessVisitPlace() {
-  let visitPlaceAnswer = prompt('Yes or no, I like to visit Los Angeles.').toLowerCase();
+// function guessVisitPlace() {
+//   let visitPlaceAnswer = prompt('Yes or no, I like to visit Los Angeles.').toLowerCase();
 
-  if (visitPlaceAnswer === 'yes') {
-    alert('Yep!');
-    score += 1;
-  } else {
-    alert('No, I like to visit Los Angeles');
-  }
-}
+//   if (visitPlaceAnswer === 'yes') {
+//     alert('Yep!');
+//     score += 1;
+//   } else {
+//     alert('No, I like to visit Los Angeles');
+//   }
+// }
 
-guessVisitPlace();
+// guessVisitPlace();
 
-function guessWhere() {
-  let whereAnswer = prompt('Am I from Seattle?').toLowerCase();
+// function guessWhere() {
+//   let whereAnswer = prompt('Am I from Seattle?').toLowerCase();
 
-  if (whereAnswer === 'yes') {
-    alert('That is correct! I live in Seattle!');
-    score += 1;
-  } else {
-    alert('No, I am from Seattle!');
-  }
-}
+//   if (whereAnswer === 'yes') {
+//     alert('That is correct! I live in Seattle!');
+//     score += 1;
+//   } else {
+//     alert('No, I am from Seattle!');
+//   }
+// }
 
-guessWhere();
+// guessWhere();
 
-function guessJob() {
-  let jobAnswer = prompt('Have I worked for a medical laboratory?').toLowerCase();
+// function guessJob() {
+//   let jobAnswer = prompt('Have I worked for a medical laboratory?').toLowerCase();
 
-  if (jobAnswer === 'yes') {
-    alert('Correct!');
-    score += 1;
-  } else {
-    alert('Nope, sorry, I did work for a medical laboratory!');
-  }
-}
+//   if (jobAnswer === 'yes') {
+//     alert('Correct!');
+//     score += 1;
+//   } else {
+//     alert('Nope, sorry, I did work for a medical laboratory!');
+//   }
+// }
 
-guessJob();
+// guessJob();
 
-function guessVacation() {
-  let sportsAnswer = prompt('Have I visited France?').toLowerCase();
+// function guessVacation() {
+//   let sportsAnswer = prompt('Have I visited France?').toLowerCase();
 
-  if (sportsAnswer === 'yes') {
-    alert('Yes!');
-    score += 1;
-  } else {
-    alert('No.');
-  }
-}
+//   if (sportsAnswer === 'yes') {
+//     alert('Yes!');
+//     score += 1;
+//   } else {
+//     alert('No.');
+//   }
+// }
 
-guessVacation();
+// guessVacation();
 
-function numberGame1(correctAnswer, guesses) {
-  for (let i = 0; i < guesses; i++) {
-    let answer = parseInt(prompt('Please Enter a number 1-10'));
-    if (answer === correctAnswer) {
-      alert('Congratulation!');
-      score += 1;
-      break;
-    } else if (answer > correctAnswer) {
-      alert('Too high! Try Again!');
-    } else if (answer < correctAnswer) {
-      alert('Too low! Try Again!');
-    }
-  }
-  alert('The answer is 3!');
-}
+// function numberGame1(correctAnswer, guesses) {
+//   for (let i = 0; i < guesses; i++) {
+//     let answer = parseInt(prompt('Please Enter a number 1-10'));
+//     if (answer === correctAnswer) {
+//       alert('Congratulations!');
+//       score += 1;
+//       break;
+//     } else if (answer > correctAnswer) {
+//       alert('Too high! Try Again!');
+//     } else if (answer < correctAnswer) {
+//       alert('Too low! Try Again!');
+//     }
+//   }
+//   alert('The answer is 3!');
+// }
 
-numberGame1(3, 4);
+// numberGame1(3, 4);
 
 function numberGameArray(answers, guess, maxGuess, isCorrectAnswer) {
-  let userAnswer = parseInt(prompt('What number am I thinking between 1-50?'));
+  let userAnswer = parseInt(prompt('Give me a number between 1 and 100.'));
+  console.log(userAnswer);
 
   while (guess < maxGuess) {
     for (let i = 0; i < answers.length; i += 1) {
+      console.log(answers[i], userAnswer);
       if (answers[i] === userAnswer) {
         isCorrectAnswer = true;
         alert('Congrats! You are correct!');
@@ -127,11 +129,11 @@ function numberGameArray(answers, guess, maxGuess, isCorrectAnswer) {
     if (isCorrectAnswer === true) {
       break;
     }
-    userAnswer = prompt('Guess again!');
+    userAnswer = parseInt(prompt('Guess again!'));
   }
   alert('Possible answers were '+answers+'.');
 }
 
-numberGameArray([2, 12, 65, 34, 98, 3], 0, 6, false);
+numberGameArray([2,12,65,34,98,3], 0, 6, false);
 
 alert('Your score was ' + score + ' out of 7!');
